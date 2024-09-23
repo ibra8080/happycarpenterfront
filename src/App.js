327d42266b6073b6f34c27a216a37e216e8f1fcf
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-09-23 15:54:55 +0000

    Create App.module.css basic styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import styles from './App.module.css'; // We'll create this CSS module file
 
 const Home = () => <h1>Welcome to Happy Carpenter</h1>;
 const Login = () => <h2>Login Page</h2>;
@@ -10,14 +11,16 @@ const Register = () => <h2>Register Page</h2>;
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className={styles.App}>
         <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
+        <main className={styles.Main}>
+          <div className={styles.Content}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
         </main>
         <Footer />
       </div>
