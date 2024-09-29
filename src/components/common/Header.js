5ba5338923f860bfb0e9d2b90b65ac10571f76ac
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-09-29 13:49:56 +0000

    Update Header.js component to handle username

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ user, onLogout }) => {
   return (
     <header>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
+          {user ? (
+            <>
+              <li>Welcome, {user.username || 'User'}!</li>
+              <li><button onClick={onLogout}>Logout</button></li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
