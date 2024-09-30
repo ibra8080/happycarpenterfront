4f06ee8f5029757a46a9efa0989cdd3f4c9b5905
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-09-30 10:23:21 +0000

    Create Header.module.css

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaHome, FaSignInAlt, FaUserPlus, FaSignOutAlt } from 'react-icons/fa';
+import logo from '../../assets/images/happycarpenterlogo.png';
+import styles from './Header.module.css';
 
 const Header = ({ user, onLogout }) => {
   return (
-    <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
-      <nav>
-        <ul style={{ listStyle: 'none', display: 'flex', gap: '20px' }}>
-          <li><Link to="/">Home</Link></li>
-          {user ? (
-            <>
-              <li style={{ display: 'flex', alignItems: 'center' }}>
-                {user.profile_image && (
-                  <img 
-                    src={user.profile_image} 
-                    alt="Profile" 
-                    style={{ width: '30px', height: '30px', borderRadius: '50%', marginRight: '10px', objectFit: 'cover' }}
-                  />
-                )}
-                 {user.username || 'User'}
-              </li>
-              <li><button onClick={onLogout}>Logout</button></li>
-            </>
-          ) : (
-            <>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/register">Register</Link></li>
-            </>
-          )}
-        </ul>
-      </nav>
-    </header>
+    <Navbar bg="light" expand="lg" className={`mb-3 ${styles.navbar}`}>
+      <Container>
+        <Navbar.Brand as={Link} to="/" className={styles.navbarBrand}>
+          <img
+            src={logo}
+            height="50"
+            className="d-inline-block align-top"
+            alt="Happy Carpenter Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" className={styles.navLink}><FaHome /> Home</Nav.Link>
+            {user ? (
+              <>
+                <Nav.Link as={Link} to="/profile" className={styles.navLink}>
+                  {user.profile_image && (
+                    <img 
+                      src={user.profile_image} 
+                      alt="Profile" 
+                      className={styles.profileImage}
+                    />
+                  )}
+                  {user.username || 'User'}
+                </Nav.Link>
+                <Nav.Link onClick={onLogout} className={styles.navLink}><FaSignOutAlt /> Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login" className={styles.navLink}><FaSignInAlt /> Login</Nav.Link>
+                <Nav.Link as={Link} to="/register" className={styles.navLink}><FaUserPlus /> Register</Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
