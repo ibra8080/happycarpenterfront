a50bbc775259ff760dc20b8d876419af01a16fc1
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-09-30 10:22:42 +0000

    Creat Footer.module.css

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2023 Happy Carpenter. All rights reserved.</p>
+    <footer className={styles.footer}>
+      <Container>
+        <p className={styles.footerText}>&copy; 2023 Happy Carpenter. All rights reserved.</p>
+      </Container>
     </footer>
   );
 };
