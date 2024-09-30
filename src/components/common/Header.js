eecb54e9826ee0ef17f83d1fcdda7d13720c3c96
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-09-30 10:49:46 +0000

    Update app, header and footer with css styles

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,7 +7,7 @@ import styles from './Header.module.css';
 
 const Header = ({ user, onLogout }) => {
   return (
-    <Navbar bg="light" expand="lg" className={`mb-3 ${styles.navbar}`}>
+    <Navbar bg="light" expand="lg" className={styles.navbar}>
       <Container>
         <Navbar.Brand as={Link} to="/" className={styles.navbarBrand}>
           <img
