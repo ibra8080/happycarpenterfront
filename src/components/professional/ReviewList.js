7512095ca2caaca0880c1147baecec714d3a42ca
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-07 09:23:48 +0000

    Create ReviewList component

---
+++ src/components/professional/ReviewList.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import axios from 'axios';
+
+const ReviewList = ({ user }) => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
+  const fetchReviews = async () => {
+    try {
+      const response = await axios.get('https://happy-carpenter-ebf6de9467cb.herokuapp.com/reviews/', {
+        headers: { Authorization: `Bearer ${user.token}` }
+      });
+      setReviews(response.data);
+    } catch (error) {
+      console.error('Error fetching reviews:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Your Reviews</h2>
+      <ListGroup>
+        {reviews.map(review => (
+          <ListGroup.Item key={review.id}>
+            <h5>Rating: {review.rating}/5</h5>
+            <p>{review.content}</p>
+            <small>By: {review.reviewer}</small>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
+  );
+};
+
+export default ReviewList;
