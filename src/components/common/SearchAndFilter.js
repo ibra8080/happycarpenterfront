30dc7d27ef4ca8c31b6b4de6b78fa4ce779b8f09
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-17 11:29:57 +0000

    Install react-bootstrap-icons package

--- src/components/common/SearchAndFilter.js
+++ src/components/common/SearchAndFilter.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
+import { ChevronDown } from 'react-bootstrap-icons';
 import styles from './SearchAndFilter.module.css';
 
 const SearchAndFilter = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filters, setFilters] = useState({
     categories: '',
-    user_type: '',
     image_filter: '',
     ordering: '-created_at'
   });
@@ -30,89 +30,86 @@ const SearchAndFilter = ({ onSearch }) => {
 
   return (
     <Form onSubmit={handleSubmit} className={styles.searchForm}>
-      <Row>
-        <Col md={6}>
+      <Row className="mb-3">
+        <Col>
           <Form.Group className={styles.searchInput}>
             <Form.Control
               type="text"
-              placeholder="Search posts..."
+              placeholder="Search..."
               value={searchTerm}
               onChange={handleSearchChange}
             />
           </Form.Group>
         </Col>
-        <Col md={6}>
-          <Form.Group className={styles.filterSelect}>
-            <Form.Control
-              as="select"
-              name="categories"
-              value={filters.categories}
-              onChange={handleFilterChange}
-            >
-              <option value="">All Categories</option>
-              <option value="furniture">Furniture</option>
-              <option value="antiques">Antiques</option>
-              <option value="renovation&repair">Renovation & Repair</option>
-              <option value="artworks">Artworks</option>
-              <option value="tools">Tools</option>
-              <option value="construction">Construction</option>
-              <option value="other">Other</option>
-            </Form.Control>
-          </Form.Group>
-        </Col>
       </Row>
       <Row>
-        <Col md={4}>
+        <Col md={3}>
           <Form.Group className={styles.filterSelect}>
-            <Form.Control
-              as="select"
-              name="user_type"
-              value={filters.user_type}
-              onChange={handleFilterChange}
-            >
-              <option value="">All Users</option>
-              <option value="amateur">Amateur</option>
-              <option value="professional">Professional</option>
-            </Form.Control>
+            <div className={styles.selectWrapper}>
+              <Form.Control
+                as="select"
+                name="categories"
+                value={filters.categories}
+                onChange={handleFilterChange}
+              >
+                <option value="">All Categories</option>
+                <option value="furniture">Furniture</option>
+                <option value="antiques">Antiques</option>
+                <option value="renovation&repair">Renovation & Repair</option>
+                <option value="artworks">Artworks</option>
+                <option value="tools">Tools</option>
+                <option value="construction">Construction</option>
+                <option value="other">Other</option>
+              </Form.Control>
+              <ChevronDown className={styles.selectIcon} />
+            </div>
           </Form.Group>
         </Col>
-        <Col md={4}>
+        <Col md={3}>
           <Form.Group className={styles.filterSelect}>
-            <Form.Control
-              as="select"
-              name="image_filter"
-              value={filters.image_filter}
-              onChange={handleFilterChange}
-            >
-              <option value="">All Image Filters</option>
-              <option value="normal">Normal</option>
-              <option value="furniture">Furniture</option>
-              <option value="antiques">Antiques</option>
-              <option value="renovation&repair">Renovation & Repair</option>
-              <option value="artworks">Artworks</option>
-              <option value="tools">Tools</option>
-              <option value="construction">Construction</option>
-              <option value="other">Other</option>
-            </Form.Control>
+            <div className={styles.selectWrapper}>
+              <Form.Control
+                as="select"
+                name="image_filter"
+                value={filters.image_filter}
+                onChange={handleFilterChange}
+              >
+                <option value="">All Images</option>
+                <option value="normal">Normal</option>
+                <option value="furniture">Furniture</option>
+                <option value="antiques">Antiques</option>
+                <option value="renovation&repair">Renovation & Repair</option>
+                <option value="artworks">Artworks</option>
+                <option value="tools">Tools</option>
+                <option value="construction">Construction</option>
+                <option value="other">Other</option>
+              </Form.Control>
+              <ChevronDown className={styles.selectIcon} />
+            </div>
           </Form.Group>
         </Col>
-        <Col md={4}>
+        <Col md={3}>
           <Form.Group className={styles.filterSelect}>
-            <Form.Control
-              as="select"
-              name="ordering"
-              value={filters.ordering}
-              onChange={handleFilterChange}
-            >
-              <option value="-created_at">Newest First</option>
-              <option value="created_at">Oldest First</option>
-              <option value="-updated_at">Recently Updated</option>
-              <option value="updated_at">Least Recently Updated</option>
-            </Form.Control>
+            <div className={styles.selectWrapper}>
+              <Form.Control
+                as="select"
+                name="ordering"
+                value={filters.ordering}
+                onChange={handleFilterChange}
+              >
+                <option value="-created_at">Newest First</option>
+                <option value="created_at">Oldest First</option>
+                <option value="-updated_at">Recently Updated</option>
+                <option value="updated_at">Least Recently Updated</option>
+              </Form.Control>
+              <ChevronDown className={styles.selectIcon} />
+            </div>
           </Form.Group>
+        </Col>
+        <Col md={3}>
+          <Button type="submit" className={styles.searchButton}>Search</Button>
         </Col>
       </Row>
-      <Button type="submit" className={styles.searchButton}>Apply Filters</Button>
     </Form>
   );
 };
