49ee1c56db1be22481d13b9882c96aa681ee2fa4
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-10-21 06:54:33 +0000

    Update Babel configuration and build process

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     presets: ['react-app'],
-    plugins: ['@babel/plugin-proposal-private-property-in-object']
+    plugins: [
+      '@babel/plugin-proposal-private-property-in-object',
+      '@babel/plugin-transform-private-property-in-object'
+    ]
   };
+  
